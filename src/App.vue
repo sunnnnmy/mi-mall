399d5da165e9a57e5b73f295c06a664e703c4d72
sunnny <1239214623@qq.com>
2022-01-09 02:22:55 +0800

    项目目录提交

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import axios from 'axios'
 import jsonp from 'jsonp'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+   
   },
   mounted(){
-    // let url="http://www.easy-mock.com/mock/5b012c1fe6e1035843vdaff/mickapi/table/list"
-    // CORS跨域
-    // axios.get(url).then(()=>{
-    // })
-    //JsonP跨域
-  //  jsonp(url,(err,res)=>{
-  //    let result =res;
-  //    this.data=result;
-  //  })
+
+     }
   // }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ font-size: 15;
 }
 </style>
