d368dab5ad7d09eef3d9af31737f270ff2640836
sunnny <1239214623@qq.com>
2022-01-10 16:28:09 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+//根据前端的跨域方式做调整  eg./a/: /api/a/b => /a/b
+axios.defaults.baseURL = '/api'; 
+axios.defaults.timeout=8000;
+/***
+ * 接口错误拦截代码
+ */
+axios.interceptors.response.use(function(response){
+  let res = response.data;
+  if (res.status == 0) {
+    return res.data;
+  }else if (res.status == 10) { //未登录
+    window.location.href = '/#/login'
+  }else{
+    alert(res.msg);
+  }
+})
+
+Vue.use(VueAxios,axios)
 Vue.config.productionTip = false
 
 new Vue({
